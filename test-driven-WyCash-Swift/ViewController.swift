3abf3990a9523dc5a24aa0bc2cd37c6af28bad57
murakamiii <9khc727ru7@gmail.com>
2017-11-11 19:02:28 +0900

    Initial Commit

---
+++ test-driven-WyCash-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-driven-WyCash-Swift
+//
+//  Created by MURAKAMI on 2017/11/11.
+//  Copyright © 2017年 MURAKAMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
